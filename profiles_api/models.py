d6d5ec6d2db69946d3b5e3d1d2721ada638a7e77
Anug Saha <anugsaha@gmail.com>
2022-01-13 21:41:40 -0500

    create user model manager

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,7 +1,31 @@
+from tkinter.ttk import PanedWindow
 from django.db import models
 
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserProfileManager(BaseUserManager): 
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError("Users must have email")
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password): 
+        user =self.create_user(email, name, password)
+
+        user.is_superuser =True
+        user.is_staff =True
+        user.save(using=self._db)
+
+        return user
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin): 
